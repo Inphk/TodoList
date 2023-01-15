c141ba84078e5d9a75e7882948b3c75364482bb8
Inphk <1074224487@qq.com>
2023-01-15 20:20:29 +0800

    学习实现接口请求验证

--- src/TodoList.Application/DependencyInjection.cs
+++ src/TodoList.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using TodoList.Application.Common.Behaviors;
 
 namespace TodoList.Application;
 
@@ -13,6 +15,9 @@ public static class DependencyInjection
         services.AddMediatR(assembly);
 
         services.AddAutoMapper(assembly);
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
         return services;
     }
